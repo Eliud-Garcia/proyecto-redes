b62cf1ffb9ef89a2c0c11903bf0a8be70bcb0da3
Eliud-Garcia <jhonniereliudgarciacapera@gmail.com>
2025-09-23 16:30:15 -0500

    git ignore

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/eliud/Escritorio/programming/redes/venv
+set -gx VIRTUAL_ENV /home/eliud/Escritorio/programming/proyecto-redes/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
